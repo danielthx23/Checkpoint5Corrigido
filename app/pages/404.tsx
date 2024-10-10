9fae37c4d66ecb09da1d50f72fbb5540b983cce9
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-10-10 11:39:58 -0400

    Not found page

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,6 +1,6 @@
 const Custom404 = () => {
     return (
-        <main className="w-screen h-screen bg-white flex flex-col justify-center items-center">
+        <main className="w-screen h-screen bg-white flex gap-10 flex-col justify-center items-center">
             <img src="https://http.cat/404" alt="imagem gato 404" />
             <h1 className="text-xl text-black font-bold">404 - Pagina não encontrada!</h1>
         </main>
