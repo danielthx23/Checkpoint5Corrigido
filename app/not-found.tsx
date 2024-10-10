f944b3160031d003a5c956d3fa3d06d5c8adb9b9
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-10-10 11:46:42 -0400

    Not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,13 @@
-const Custom404 = () => {
+import Link from "next/link"
+
+const NotFound = () => {
     return (
         <main className="w-screen h-screen bg-white flex gap-10 flex-col justify-center items-center">
             <img src="https://http.cat/404" alt="imagem gato 404" />
             <h1 className="text-xl text-black font-bold">404 - Pagina não encontrada!</h1>
+            <Link href="/">Voltar para home</Link>
         </main>
     ) 
 }
+
+export default NotFound
