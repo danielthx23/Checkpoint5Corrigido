007e4ce81e9a630a7773207dcaaf344cb20f6f00
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-10-10 11:36:15 -0400

    404

---
+++ app/404.tsx
@@ -0,0 +1,8 @@
+const Custom404 = () => {
+    return (
+        <main className="w-screen h-screen bg-white flex flex-col justify-center items-center">
+            <img src="https://http.cat/404" alt="imagem gato 404" />
+            <h1 className="text-xl text-black font-bold">404 - Pagina não encontrada!</h1>
+        </main>
+    ) 
+}
