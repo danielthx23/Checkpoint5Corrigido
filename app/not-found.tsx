32c0a711ad5f84d060a46e67c213eadd848a28bd
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-10-10 11:57:53 -0400

    Update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const notfound = () => {
         <main className="w-screen h-screen bg-white flex gap-10 flex-col justify-center items-center">
             <img src="https://http.cat/404" alt="imagem gato 404" />
             <h1 className="text-xl text-black font-bold">404 - Pagina não encontrada!</h1>
-            <Link className="bg-blue p-4 py-2 text-white" href="/">Voltar para home</Link>
+            <Link className="bg-blue-500 p-4 py-2 text-white" href="/">Voltar para home</Link>
         </main>
     ) 
 }
