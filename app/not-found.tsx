bba3a089870e115ac0852b32fb1d71d3805d63d1
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-10-10 11:48:51 -0400

    Not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 
-const NotFound = () => {
+const notfound = () => {
     return (
         <main className="w-screen h-screen bg-white flex gap-10 flex-col justify-center items-center">
             <img src="https://http.cat/404" alt="imagem gato 404" />
             <h1 className="text-xl text-black font-bold">404 - Pagina não encontrada!</h1>
-            <Link href="/">Voltar para home</Link>
+            <Link className="bg-blue p-4 py-2 text-white" href="/">Voltar para home</Link>
         </main>
     ) 
 }
 
-export default NotFound
+export default notfound
